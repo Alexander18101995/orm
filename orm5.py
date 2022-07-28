4e02e45c3b00ff7fc55a9f81508985060b87136b
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-07-28 16:19:13 +0300

    Add files via upload

--- orm5.py
+++ orm5.py
@@ -11,51 +11,52 @@ session = Session()
 
 Base = declarative_base()
 
-class publisher(Base):
+class Publisher(Base):
     __tablename__ = 'publisher'
-
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(lenght=40))
 
-    pub = relationship('book', back_population='b')
-
-class book(Base):
-    __tablename__ = 'book'
-
-    id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String(lenght=40))
-    id_publisher = sq.Column(sq.Integer, unique=True)
-
-    b = relationship('publisher',back_population='pub')
-    bs = relationship('stock',back_population='sb')
-class shop(Base):
+class Shop(Base):
     __tablename__ = 'shop'
-
     id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(lenght=40))
+    name = sq.Column(sq.String(length=40), unique=True)
 
-    shop = relationship('stock', back_population='stock')
+class Book(Base):
+    __tablename__ = 'book'
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=40), unique=True)
+    publisher_id = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'))
 
-class stock(Base):
+    publisher = relationship(Publisher, backref='book')
+
+class Stock(Base):
     __tablename__ = 'stock'
-
     id = sq.Column(sq.Integer, primary_key=True)
-    id_book = sq.Column(sq.Integer,unique=True)
-    id_shop = sq.Column(sq.Integer, unique=True)
+    book_id = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    shop_id = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
     count = sq.Column(sq.Integer)
 
-    stock = relationship('shop', back_population='shop')
-    sb = relationship('book', back_population='bs')
-    s = relationship('sale', back_population='sale')
-class sale(Base):
-    __tablename__ = 'sale'
+    book = relationship(Book, backref='stock')
+    shop = relationship(Shop, backref='stock')
 
+class Sale(Base):
+    __tablename__ = 'sale'
     id = sq.Column(sq.Integer, primary_key=True)
     price = sq.Column(sq.Integer)
     date_sale = sq.Column(sq.Date)
-    id_stock = sq.Column(sq.Integer)
+    stock_id = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
     count = sq.Column(sq.Integer)
 
-    sale = relationship('stock', back_population='s')
+    stock = relationship(Stock, backref='sale')
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create.all(engine)
+create_tables(engine)
+
+res = input('id...')
+query_1 = session.query(Publisher).filter(Publisher.id == res)
+for i in query_1.all():
+    print(f'{res}-{i.name}')
 
 session.close()
